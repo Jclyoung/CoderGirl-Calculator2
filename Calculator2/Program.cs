c7054a09f6f123ea301cbe4da8eb80f83898e97e
Jennifer Young <jclyoung@gmail.com>
2019-03-24 16:08:29 -0500

    Completed

--- Calculator2/Program.cs
+++ Calculator2/Program.cs
@@ -6,16 +6,69 @@ namespace Calculator2
     {
         public static void Main()
         {
-            double result = 0;
-
             // Create a simple calculator that asks the user to input two different numbers 
             // and whether they want to add, subtract, multiply or divide them. 
+            /// Ask the first number user would like to use
+            /// proceed to convert into a usable double
+            Console.WriteLine("Please enter your first number:");
+            string firstNumberInput = Console.ReadLine();
+            firstNumberInput = firstNumberInput.Trim();
+            double firstNumber;
+            if (!double.TryParse(firstNumberInput, out firstNumber))
+                Console.WriteLine($"The value {firstNumberInput} is not a valid number.");
+
+            Console.WriteLine("Please enter your second number:");
+            string secondNumberInput = Console.ReadLine();
+            secondNumberInput = secondNumberInput.Trim();
+            double secondNumber;
+            if (!double.TryParse(secondNumberInput, out secondNumber))
+                Console.WriteLine($"The value {secondNumberInput} is not a valid number.");
+
+            Console.WriteLine("Please enter if you would like to add, subtract, multiply or divide: ");
+            string operation = Console.ReadLine();
+
+            if (operation == "add")
+                Add(firstNumber, secondNumber); /// Method to add 2 user numbers together
+            if (operation == "subtract")
+                Subtract(firstNumber, secondNumber); /// Method to subtract 2 user numbers
+            if (operation == "multiply")
+                Multiply(firstNumber, secondNumber); /// Method to multiply 2 user numbers together
+            if (operation == "divide")
+                Divide(firstNumber, secondNumber); /// Method to divide 2 user numbers
+            Console.ReadLine();
+
 
             // Use methods for each type of calculation.
+            // Create a simple calculator that asks the user to input two different numbers            
 
-            Console.WriteLine($"Total = {result}");
+            // and whether they want to add, subtract, multiply or divide them. 
+        }
+        public static double Add(double firstNumber, double secondNumber)
+        {
+            double answer = 0;
+            answer = (firstNumber + secondNumber);
+            return answer;
+        }
 
-            Console.ReadLine();
+        public static double Subtract(double firstNumber, double secondNumber)
+        {
+            double answer = 0;
+            answer = (firstNumber - secondNumber);
+            return answer;
+        }
+
+        public static double Multiply(double firstNumber, double secondNumber)
+        {
+            double answer = 0;
+            answer = (firstNumber * secondNumber);
+            return answer;
+        }
+
+        public static double Divide(double firstNumber, double secondNumber)
+        {
+            double answer = 0;
+            answer = (firstNumber / secondNumber);
+            return answer;
         }
     }
 }
